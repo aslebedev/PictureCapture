fb2a95ec8c9a563b8ab0872c072fd87c707e2432
Alexander Lebedev <alexander.s.lebedev@gmail.com>
2019-11-14 10:43:48 +0700

    Initial Commit

---
+++ PictureCapture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PictureCapture
+//
+//  Created by alexander on 14.11.2019.
+//  Copyright © 2019 alexander. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
